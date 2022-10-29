b7005d0c42bce71865a1e96c9cb5dff7b3ed4fd5
Bogdan-Yanish <106109430+Bogdan-Yanish@users.noreply.github.com>
2022-10-29 22:41:39 +0300

    create new function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -29,3 +29,7 @@ function stopColorRandom() {
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   };
+
+  function getRandomHexColorTrue() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
