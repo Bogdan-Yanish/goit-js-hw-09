6db5f747aa758dc5cd047b025a948b21b72666d1
Bogdan-Yanish <106109430+Bogdan-Yanish@users.noreply.github.com>
2022-10-29 22:32:45 +0300

    create color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startColorBtn: document.querySelector('[data-start]'),
+    stopColorBtn: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+  
 
+refs.startColorBtn.addEventListener('click', startChangeColorRandom);
+refs.stopColorBtn.addEventListener('click', stopColorRandom);
+
+function startChangeColorRandom() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.startColorBtn.disabled = true;
+    refs.stopColorBtn.disabled = false; 
+
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+};
+
+function stopColorRandom() {
+    clearInterval(timerId);
+    // document.body.style.backgroundColor = '';
+    refs.startColorBtn.disabled = false;   
+    refs.stopColorBtn.disabled = true;   
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
